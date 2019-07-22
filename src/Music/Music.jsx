d5fffaccf56dfe5d1ae95d287a95a8d2b75a4719
Benedikt89 <constantine.kostka@gmail.com>
2019-07-22 15:58:38 +0300

    +Links

---
+++ src/Music/Music.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Music.module.css';
+
+
+const Music = () => {
+    return (
+        <div>
+            MUSIC
+        </div>
+    );
+}
+
+
+export default Music;
---
+++ src/Settings/Settings.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Settings.module.css';
+
+
+const Settings = () => {
+    return (
+        <div>
+            SETTINGS
+        </div>
+    );
+}
+
+
+export default Settings;
